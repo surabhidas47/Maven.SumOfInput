2e714410306255cf057ee9b4d01f8c098071be8d
surabhi <surabhi.das@outlook.com>
2023-06-30 14:05:18 -0400

    okay

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,33 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args){
-        
-        int number = getInputNumber();
-        
-        int sum = sumOfNumbers(number);
-        
-        System.out.println(sum);
+    public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        int number;
+        int sum=0;
+        int i;
+
+        System.out.println("Enter your number!");
+        number = input.nextInt(); //reading number from scanner
+
+        for (i = 1; i <= number; i++) { //this starts counting at 1 and stop when it reaches 'number' and adds by 1
+
+            sum += i; //this adds i to sum as it increases by 1
+
+        }
+        System.out.println(sum);
     }
+
+}
+
+
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
-    }
-    
-    
-    int getInputNumber() {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter any number: ");
 
-        // This method reads the number provided using keyboard
-        int num = scan.nextInt();
 
-        // Closing Scanner after the use
-        scan.close();
-        return num;
-    }
     
-    
-    
-    // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
-    }
-   
-    // for Extra extra credit - compare the methods and show which one is faster
-    // google "how to time some java code"
-    // use "System.currentTimeMillis()" to ask the system what time it is.
-}
